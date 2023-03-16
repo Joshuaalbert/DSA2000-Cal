644c58d73bb9d4ee1af8a7b68a31b4796549ad07
joshuaalbert <albert@strw.leidenuniv.nl>
2023-03-15 21:09:18 -0600

    * correct

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml down
 
-docker compose -f "$SCRIPT_DIR"/docker-compose.yaml up -d quartical
+docker compose -f "$SCRIPT_DIR"/docker-compose.yaml up -d --force-recreate quartical
 
 # This streams the logs. Push ctrl-C to detach from logs.
 docker compose logs -f
