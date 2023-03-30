01d3639a68f6de26f334bdf469578a87ace9fbcf
joshuaalbert <albert@strw.leidenuniv.nl>
2023-03-30 15:36:51 -0400

    * Update

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml down
 
+# We force rebuild to take into account edits to changes to .env variables between runs.
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml build --no-cache quartical
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml up -d --force-recreate quartical
 
