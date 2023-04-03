453ade9e78d9385b34f49e5442d043522af4b910
joshuaalbert <albert@strw.leidenuniv.nl>
2023-04-02 22:02:34 -0400

    * Update

--- services/inspect/src/entrypoint.sh
+++ services/inspect/src/entrypoint.sh
@@ -7,7 +7,7 @@ set -x
 
 # Start up a jupyter notebook which will be useful even while it's running
 
-jupyter notebook --notebook-dir=notebooks --port=8888 --no-browser --allow-root \
-  --NotebookApp.allow_origin='*' --NotebookApp.ip='0.0.0.0' --NotebookApp.notebook_dir=notebooks \
+jupyter notebook --port=8888 --no-browser --allow-root --NotebookApp.allow_origin='*' --NotebookApp.ip='0.0.0.0' \
+  --NotebookApp.notebook_dir=notebooks \
   --NotebookApp.token="$JUPYTER_TOKEN" --NotebookApp.allow_password_change="False" \
   --NotebookApp.password=""
