eb549d98033f43bbf9b7242f880898b8e9687ce1
joshuaalbert <albert@strw.leidenuniv.nl>
2023-04-03 10:04:17 -0400

    * Update

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 # gets the script dir, should work with bash <script> and source <script>
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-docker compose -f "$SCRIPT_DIR"/docker-compose.yaml down quartical
-
 # We force rebuild to take into account edits to changes to .env variables between runs.
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml build --no-cache quartical
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml up -d --force-recreate quartical
