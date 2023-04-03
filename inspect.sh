0be14c87d9a12269421064d3a586e5e1e5ba7a30
joshuaalbert <albert@strw.leidenuniv.nl>
2023-04-03 08:53:19 -0400

    * Update

--- inspect.sh
+++ inspect.sh
@@ -3,7 +3,7 @@
 # gets the script dir, should work with bash <script> and source <script>
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-docker compose -f "$SCRIPT_DIR"/docker-compose.yaml down
+docker compose -f "$SCRIPT_DIR"/docker-compose.yaml down inspect
 
 # We force rebuild to take into account edits to changes to .env variables between runs.
 docker compose -f "$SCRIPT_DIR"/docker-compose.yaml build --no-cache inspect
