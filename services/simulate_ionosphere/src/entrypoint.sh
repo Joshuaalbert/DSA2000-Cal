43eb82b0ba4ba2f3fa6b7ccc8dee662d679bcc35
joshuaalbert <albert@strw.leidenuniv.nl>
2023-04-10 17:55:59 -0400

    * Update

---
+++ services/simulate_ionosphere/src/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Run from /dsa/input
+
+set -euo pipefail
+set -x
+
+# Run Simulate Ionosphere
+
+RUN_DIR=/dsa/run/sim_dsa2000W_1000m_grid_"$DURATION"_"$RESOLUTION"
+mkdir -p $RUN_DIR
+
+ln -sf $RUN_DIR /dsa/output                                                  # for output simulation
+OUTPUT_NAME=/dsa/output/sim_dsa2000W_1000m_grid_"$DURATION"_"$RESOLUTION".h5 # output h5parm
+
+python /dsa/code/bayes_gain_screens/simulate_ionosphere_phase_screen.py \
+  --output_h5parm="$OUTPUT_NAME" \
+  --phase_tracking='00h00m0.0s +37d07m47.400s' \
+  --array_name=dsa2000W_1000m_grid \
+  --start_time='2019-03-19T19:58:14.9' \
+  --time_resolution="$RESOLUTION" \
+  --duration="$DURATION" \
+  --field_of_view_diameter=4.0 \
+  --avg_direction_spacing=10000.0 \
+  --specification="simple"
+
+# Followup with interpolating onto DSA2000 array
