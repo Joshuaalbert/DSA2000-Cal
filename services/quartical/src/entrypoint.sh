489960259f437112bb3bda86f066e359ad5f6ee5
joshuaalbert <albert@strw.leidenuniv.nl>
2023-05-11 22:20:13 +0200

    * Update

--- services/quartical/src/entrypoint.sh
+++ services/quartical/src/entrypoint.sh
@@ -7,24 +7,7 @@ set -x
 
 ln -s $PWD/$RUN_NAME $PWD/input
 
-## These should match parset.yaml
-SKY_MODEL_BBS=input/skymodel.bbs
-SKY_MODEL_TIGGER=input/skymodel.lsm.html
-
-# Check if SKY_MODEL_TIGGER doesn't exist
-if [[ ! -f "$SKY_MODEL_TIGGER" ]]; then
-
-  # Check if SKY_MODEL_BBS exists
-  if [[ ! -f "$SKY_MODEL_BBS" ]]; then
-    echo "$SKY_MODEL_BBS doesn't exist. Aborting conversion."
-    exit 1
-  fi
-
-  # Convert SKY_MODEL_BBS to SKY_MODEL_TIGGER
-  tigger-convert -t BBS -o Tigger $SKY_MODEL_BBS $SKY_MODEL_TIGGER
-fi
-
-# Run Quartical
+## Prepare run dir
 
 MS_NAME=$(basename "$MS_NAME")
 RUN_NAME_MS=$(echo "$MS_NAME" | rev | cut -f2- -d'.' | rev)
@@ -32,12 +15,27 @@ RUN_DIR=/dsa/run/$RUN_NAME_MS/$RUN_NAME
 mkdir -p $RUN_DIR
 
 # Links to standard names for parset.yaml to use
-ln -sf $RUN_DIR /dsa/output                   # for output solutions and logs
+ln -sf $RUN_DIR /dsa/output                    # for output solutions and logs
 ln -sf /dsa/data/$MS_NAME /dsa/output/input.ms # for ms input
 
-# for clarity
+# for clarity put sky models in output
 cp input/parset.yaml /dsa/output
-cp $SKY_MODEL_TIGGER /dsa/output
+
+## These should match input_model.recipe in the parset.yaml
+SKY_MODEL_BBS_FILES="input/skymodel*.bbs"
+
+for SKY_MODEL_BBS in $SKY_MODEL_BBS_FILES; do
+  # Replace the file .bbs suffix with .lsm.html
+  SKY_MODEL_TIGGER="${SKY_MODEL_BBS%.bbs}.lsm.html"
+
+  # store it in run dir for posterity
+  cp $SKY_MODEL_TIGGER /dsa/output
+
+  # Run the convert script
+  convert-script "$SKY_MODEL_BBS" "$SKY_MODEL_TIGGER"
+done
+
+# Run Quartical
 
 cd /dsa/output
 
