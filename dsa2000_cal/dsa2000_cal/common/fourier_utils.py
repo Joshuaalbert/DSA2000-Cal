00c540c6fc9fd62f1070d4f6838890df178d6ff1
joshuaalbert <albert@strw.leidenuniv.nl>
2024-04-16 14:04:26 +0200

    * optimal fft size

--- dsa2000_cal/dsa2000_cal/common/fourier_utils.py
+++ dsa2000_cal/dsa2000_cal/common/fourier_utils.py
@@ -61,3 +61,85 @@ class ApertureTransform:
     def _to_image_casa(self, f_aperture, axes, dx):
         # uses +2pi convention so ifft is used
         return jnp.fft.fftshift(jnp.fft.fftn(jnp.fft.ifftshift(f_aperture, axes=axes), axes=axes), axes=axes) * dx
+
+
+def _find_optimal_fft_size(N, required_radix=[2]):
+    # Allowed radix bases in the order of efficiency
+    allowed_radix = [4, 2, 3, 5, 7]
+
+    def is_optimal_size(x):
+        """ Check if x can be entirely factorized using the allowed radix bases """
+        for radix in allowed_radix:
+            while x % radix == 0:
+                x //= radix
+        return x == 1
+
+    def includes_required_radix(x):
+        """ Check if x includes all required radix factors at least once """
+        for radix in required_radix:
+            temp = x
+            if temp % radix != 0:
+                return False
+        return True
+
+    # Ensure N includes all required radix factors
+    for radix in required_radix:
+        while N % radix != 0:
+            N *= radix
+
+    # Start checking from N, increment until an optimal size is found
+    while not (is_optimal_size(N) and includes_required_radix(N)):
+        N += 1
+
+    return N
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+def find_optimal_fft_size(N, required_radix=None,
+                          allowed_radix=None) -> int:
+    """
+    Find the optimal size for FFT given the input size N and required radix factors.
+
+    Args:
+        N: the input size
+        required_radix: the required radix factors
+        allowed_radix: the allowed radix factors
+
+    Returns:
+        int: the optimal size for FFT
+    """
+    if required_radix is None:
+        required_radix = {4}  # Most efficienct radix
+    if allowed_radix is None:
+        allowed_radix = {4, 2, 3, 5, 7}
+
+    if not required_radix.issubset(allowed_radix):
+        raise ValueError("required_radix must be a subset of allowed_radix")
+
+    def satisfy(x):
+        factors = prime_factors(x)
+        # Since 4 is good, replace 2 2's with 4
+        if factors.count(2) == 2:
+            factors.remove(2)
+            factors.remove(2)
+            factors.append(4)
+        factors = set(factors)
+        # only contains allows, and must contain required
+        return factors.issubset(allowed_radix) and required_radix.issubset(factors)
+
+    while not satisfy(N):
+        N += 1
+    return N
