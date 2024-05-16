9696b55a93b3a5191aaa1367338609e475acdee0
joshuaalbert <albert@strw.leidenuniv.nl>
2024-05-16 16:19:33 +0200

    Do #64

---
+++ dsa2000_cal/dsa2000_cal/assets/source_models/tau_a/source_model.py
@@ -0,0 +1,17 @@
+import glob
+import os
+from typing import List
+
+from dsa2000_cal.assets.registries import source_model_registry
+from dsa2000_cal.assets.source_models.source_model import AbstractWSCleanSourceModel
+
+
+@source_model_registry(template='tau_a')
+class TauASourceModel(AbstractWSCleanSourceModel):
+
+    def get_wsclean_clean_component_file(self) -> str:
+        return os.path.join(*self.content_path, 'Tau-sources.txt')
+
+    def get_wsclean_fits_files(self) -> List[str]:
+        fits_files = glob.glob(os.path.join(*self.content_path, 'fits_models', 'Tau-*-model.fits'))
+        return fits_files
---
+++ dsa2000_cal/dsa2000_cal/assets/source_models/vir_a/source_model.py
@@ -0,0 +1,17 @@
+import glob
+import os
+from typing import List
+
+from dsa2000_cal.assets.registries import source_model_registry
+from dsa2000_cal.assets.source_models.source_model import AbstractWSCleanSourceModel
+
+
+@source_model_registry(template='vir_a')
+class TauASourceModel(AbstractWSCleanSourceModel):
+
+    def get_wsclean_clean_component_file(self) -> str:
+        return os.path.join(*self.content_path, 'Vir-sources.txt')
+
+    def get_wsclean_fits_files(self) -> List[str]:
+        fits_files = glob.glob(os.path.join(*self.content_path, 'fits_models', 'Vir-*-model.fits'))
+        return fits_files
