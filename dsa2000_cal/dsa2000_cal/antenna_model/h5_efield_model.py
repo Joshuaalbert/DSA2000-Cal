* Correct Jones calculation * improve unittests for e-field conversion

dsa2000_cal/dsa2000_cal/antenna_model/h5_efield_model.py
@@ -21,7 +21,6 @@ def convert_spherical_e_field_to_cartesian(e_phi, e_theta, phi, theta):
     Returns:
         The cartesian electric field components.
     """
-    e_phi, e_theta, phi, theta = np.broadcast_arrays(e_phi, e_theta, phi[None, None, :], theta[None, :, None])
     # Calculate the cartesian electric field components
     e_x = e_theta * np.cos(theta) * np.cos(phi) - e_phi * np.sin(phi)
     e_y = e_theta * np.cos(theta) * np.sin(phi) + e_phi * np.cos(phi)
@@ -58,9 +57,9 @@ class H5AntennaModelV1(AltAzAntennaModel):
         print_h5_structure(self.beam_file)
         with tb.open_file(self.beam_file, 'r') as file:
             # self.freqs = file.get_node("/Freq(Hz)").read() * au.Hz
-            self.freqs = file.root.freq_Hz.read() * au.Hz
-            self.theta = file.root.theta_pts.read() * au.rad
-            self.phi = file.root.phi_pts.read() * au.rad
+            self.freqs = file.root.freq_Hz.read() * au.Hz  # [num_freqs]
+            self.theta = file.root.theta_pts.read() * au.rad  # [num_theta]
+            self.phi = file.root.phi_pts.read() * au.rad  # [num_phi]
 
             # e_field_X_theta = file.get_node("/X-pol_Efields/etheta").read()  # [freq, theta, phi]
             e_field_X_theta = file.root.X_pol_Efields.etheta.read()  # [freq, theta, phi]
@@ -77,15 +76,15 @@ class H5AntennaModelV1(AltAzAntennaModel):
             e_field_Y_phi = np.transpose(e_field_Y_phi, (1, 2, 0))  # [theta, phi, freq]
 
             E_x_X_dipole, E_y_X_dipole, _ = convert_spherical_e_field_to_cartesian(
-                e_field_X_theta, e_field_X_phi, self.theta, self.phi
+                *np.broadcast_arrays(e_field_X_phi, e_field_X_theta, self.phi[None, :, None], self.theta[:, None, None])
             )  # [theta, phi, freq]
             E_x_Y_dipole, E_y_Y_dipole, _ = convert_spherical_e_field_to_cartesian(
-                e_field_Y_theta, e_field_Y_phi, self.theta, self.phi
+                *np.broadcast_arrays(e_field_Y_phi, e_field_Y_theta, self.phi[None, :, None], self.theta[:, None, None])
             )  # [theta, phi, freq]
 
             jones = np.transpose(np.asarray([[E_x_X_dipole, E_y_X_dipole],
                                              [E_x_Y_dipole, E_y_Y_dipole]]),
-                                 (2, 3, 4, 0, 1)) # [theta, phi, freq, 2, 2]
+                                 (2, 3, 4, 0, 1))  # [theta, phi, freq, 2, 2]
 
             self.amplitude = np.abs(jones) * au.dimensionless_unscaled
             self.phase = np.angle(jones) * au.rad

dsa2000_cal/dsa2000_cal/antenna_model/tests/test_efield_model.py
@@ -3,21 +3,26 @@ import numpy as np
 from dsa2000_cal.antenna_model.h5_efield_model import convert_spherical_e_field_to_cartesian
 
 
-def test_basic_conversions():
-    # Edge case where theta = 0, phi = 0 ==> E_cartesian = [0, 1, 0]
-    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(1, 0, 0, 0)
-    np.testing.assert_allclose(e_x, 0, atol=1e-8)
-    np.testing.assert_allclose(e_y, 1, atol=1e-8)
-    np.testing.assert_allclose(e_z, 0, atol=1e-8)
+def test_convert_spherical_e_field_to_cartesian():
+    # L-M frame is the same as (-Y)-X frame, i.e. Y is -L and X is M.
 
-    # Edge case where theta = pi/2, phi = pi/2 ==> E_cartesian = [-1, 0, -1]
-    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(1, 1, np.pi / 2, np.pi / 2)
-    np.testing.assert_allclose(e_x, -1, atol=1e-8)
-    np.testing.assert_allclose(e_y, 0, atol=1e-8)  # Corrected expectation
-    np.testing.assert_allclose(e_z, -1, atol=1e-8)
+    # theta=pi/2 for all phi (Looking at horizon): -theta-hat == Bore-sight
+    for phi in [0., np.pi / 2, np.pi, 3 * np.pi / 2]:
+        e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(0, -1, phi=phi, theta=np.pi / 2)
+        np.testing.assert_allclose([e_x, e_y, e_z], [0, 0, 1], atol=1e-8)
 
-    # Edge case where theta = pi, phi = pi ==> E_cartesian = [1, -1, 0]
-    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(1, 1, np.pi, np.pi)
-    np.testing.assert_allclose(e_x, 1, atol=1e-8)
-    np.testing.assert_allclose(e_y, -1, atol=1e-8)
-    np.testing.assert_allclose(e_z, 0, atol=1e-8)
+    # At theta=pi/2 phi=0 (Looking North): phi-hat == y-hat
+    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(1, 0, phi=0, theta=np.pi / 2)
+    np.testing.assert_allclose([e_x, e_y, e_z], [0, 1, 0], atol=1e-8)
+
+    # At theta=pi/2 phi=pi (Looking South): phi-hat == -y-hat
+    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(1, 0, phi=np.pi, theta=np.pi / 2)
+    np.testing.assert_allclose([e_x, e_y, e_z], [0, -1, 0], atol=1e-8)
+
+    # When theta=0, phi=0 (Looking down bore): theta-hat == x-hat
+    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(0, 1, phi=0, theta=0)
+    np.testing.assert_allclose([e_x, e_y, e_z], [1, 0, 0], atol=1e-8)
+
+    # When theta=0, phi=pi/2 (Looking down bore): theta-hat == y-hat
+    e_x, e_y, e_z = convert_spherical_e_field_to_cartesian(0, 1, phi=np.pi / 2, theta=0)
+    np.testing.assert_allclose([e_x, e_y, e_z], [0, 1, 0], atol=1e-8)

dsa2000_cal/dsa2000_cal/gain_models/tests/test_beam_gain_model.py
@@ -6,23 +6,43 @@ from dsa2000_cal.common.coord_utils import lmn_to_icrs
 from dsa2000_cal.gain_models.beam_gain_model import lmn_from_phi_theta, BeamGainModel, beam_gain_model_factory
 
 
-def test_phi_theta_to_cartesian():
+def test_lmn_from_phi_theta():
     # L = -Y, M = X, N = Z
 
+    # Bore-sight
     phi = 0.
     theta = 0.
     lmn = lmn_from_phi_theta(phi, theta)
     np.testing.assert_allclose(lmn, [0, 0, 1], atol=5e-8)
 
     phi = np.pi / 2.
+    theta = 0.
+    lmn = lmn_from_phi_theta(phi, theta)
+    np.testing.assert_allclose(lmn, [0, 0, 1], atol=5e-8)
+
+    # Points to right on sky == -L
+    phi = np.pi / 2.
     theta = np.pi / 2.
     lmn = lmn_from_phi_theta(phi, theta)
     np.testing.assert_allclose(lmn, [-1, 0, 0], atol=5e-8)
 
+    # Points to left on sky == L
+    phi = - np.pi / 2.
+    theta = np.pi / 2.
+    lmn = lmn_from_phi_theta(phi, theta)
+    np.testing.assert_allclose(lmn, [1, 0, 0], atol=5e-8)
+
+    # Points up on sky == M
     phi = 0.
     theta = np.pi / 2.
     lmn = lmn_from_phi_theta(phi, theta)
     np.testing.assert_allclose(lmn, [0, 1, 0], atol=5e-8)
+
+    # Points down on sky == -M
+    phi = np.pi
+    theta = np.pi / 2.
+    lmn = lmn_from_phi_theta(phi, theta)
+    np.testing.assert_allclose(lmn, [0, -1, 0], atol=1e-7)
 
 
 def test_beam_gain_model():