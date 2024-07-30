f887e3d33559eb348255fa9b78cbbbfda3c0f28f
joshuaalbert <albert@strw.leidenuniv.nl>
2024-07-30 20:33:47 +0200

    * Beam model factory from MS

--- dsa2000_cal/dsa2000_cal/gain_models/beam_gain_model.py
+++ dsa2000_cal/dsa2000_cal/gain_models/beam_gain_model.py
@@ -6,6 +6,7 @@ from astropy import units as au, time as at
 from dsa2000_cal.assets.content_registry import fill_registries, NoMatchFound
 from dsa2000_cal.assets.registries import array_registry
 from dsa2000_cal.gain_models.spherical_interpolator import SphericalInterpolatorGainModel
+from dsa2000_cal.measurement_sets.measurement_set import MeasurementSet
 
 
 @dataclasses.dataclass(eq=False)
@@ -13,12 +14,29 @@ class BeamGainModel(SphericalInterpolatorGainModel):
     ...
 
 
-def beam_gain_model_factory(array_name: str, full_stokes: bool = True, **kwargs) -> BeamGainModel:
+def beam_gain_model_factory(ms: MeasurementSet) -> BeamGainModel:
+    return build_beam_gain_model(
+        array_name=ms.meta.array_name,
+        model_times=ms.meta.times,
+        full_stokes=ms.is_full_stokes()
+    )
+
+
+def build_beam_gain_model(
+        array_name: str,
+        full_stokes: bool = True,
+        model_times: at.Time | None = None,
+        **kwargs
+) -> BeamGainModel:
     fill_registries()
     try:
         array = array_registry.get_instance(array_registry.get_match(array_name))
     except NoMatchFound as e:
         raise ValueError(f"Array {array_name} not found in registry. Add it to use the BeamGainModel factory.") from e
+
+    if model_times is None:
+        # Mock, else compute over time in observation
+        model_times = at.Time.now().reshape((1,))
 
     dish_model = array.get_antenna_model()
 
@@ -35,14 +53,18 @@ def beam_gain_model_factory(array_name: str, full_stokes: bool = True, **kwargs)
     voltage_gain = dish_model.get_voltage_gain()  # [num_freqs]
     amplitude = au.Quantity(amplitude / voltage_gain[:, None, None])  # [num_theta, num_phi, num_freqs, 2, 2]
 
-    # Mock, else compute over time in observation
-    model_times = at.Time.now().reshape((1,))
-
     phase = dish_model.get_phase()  # [num_theta, num_phi, num_freqs, 2, 2]
     gains = amplitude * np.exp(1j * phase.to('rad').value)  # [num_theta, num_phi, num_freqs, 2, 2]
+
+    # Right now all beams are assumed stationary over time, so we can simply put the same gain for all times.
     gains = gains.reshape(
-        (len(model_times), len(model_phi), len(model_freqs), 2, 2)
-    ) * au.dimensionless_unscaled  # [num_times, num_dir, num_freqs, 2, 2]
+        (len(model_phi), len(model_freqs), 2, 2)
+    ) * au.dimensionless_unscaled  # [num_dir, num_freqs, 2, 2]
+
+    # Repeat twice to set the same gain for all times
+    gains = au.Quantity(np.repeat(gains[None, ...], 2, axis=0))  # [num_times, num_dir, num_freqs, 2, 2]
+    # Just to min and max times
+    model_times = at.Time([model_times.tt.min(), model_times.tt.max()])
 
     if not full_stokes:
         gains = gains[..., 0, 0]
