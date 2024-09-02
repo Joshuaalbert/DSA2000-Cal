34471b2250f26994b6b0f62ac8ff30a81b5c7c86
joshuaalbert <albert@strw.leidenuniv.nl>
2024-09-02 16:31:35 +0200

    * Fix for inside JIT

--- dsa2000_cal/dsa2000_cal/common/bit_context.py
+++ dsa2000_cal/dsa2000_cal/common/bit_context.py
@@ -1,32 +1,82 @@
 import jax
+from jax import numpy as jnp
+from jax._src import dtypes
+
+original_jnp_array = jnp.array
+original_jnp_asarray = jnp.asarray
+
+
+def is_int_dtype(dtype):
+    return dtype in [jnp.int32, jnp.int64, int]
+
+
+def is_float_dtype(dtype):
+    return dtype in [jnp.float32, jnp.float64, float]
+
+
+def is_bool_type(dtype):
+    return dtype in [jnp.bool_, bool]
+
+
+def is_complex_dtype(dtype):
+    return dtype in [jnp.complex64, jnp.complex128, complex]
 
 
 class BitContext:
+    current_context = None
 
     def __init__(self, bits: int):
         if bits not in [32, 64]:
             raise ValueError("Bits must be either 32 or 64.")
         self.bits = bits
-        self.previous_setting = jax.config.read("jax_enable_x64")
+        if not jax.config.read('jax_enable_x64'):
+            raise ValueError("JAX x64 must be enabled for this context.")
 
     def __enter__(self):
-        if self.bits == 64:
-            jax.config.update("jax_enable_x64", True)
-        else:
-            jax.config.update("jax_enable_x64", False)
+        self.previous_context = BitContext.current_context
+        BitContext.current_context = self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # Restore the previous setting
-        jax.config.update("jax_enable_x64", self.previous_setting)
+        BitContext.current_context = self.previous_context
 
 
-class BitContext32(BitContext):
+def get_default_dtype(object):
+    """Determine the default dtype based on the current bit context."""
+    if BitContext.current_context is not None:
+        dtype = dtypes.canonicalize_dtype(jnp.result_type(object))
+        if BitContext.current_context.bits == 64:
+            if is_int_dtype(dtype):
+                return jnp.int64
+            elif is_float_dtype(dtype):
+                return jnp.float64
+            elif is_bool_type(dtype):
+                return jnp.bool_
+            elif is_complex_dtype(dtype):
+                return jnp.complex128
+        else:
+            if is_int_dtype(dtype):
+                return jnp.int32
+            elif is_float_dtype(dtype):
+                return jnp.float32
+            elif is_bool_type(dtype):
+                return jnp.bool_
+            elif is_complex_dtype(dtype):
+                return jnp.complex64
+    return None
 
-    def __init__(self):
-        super().__init__(32)
+
+def patched_array(object, dtype=None, **kwargs):
+    if dtype is None:  # Only override if dtype is not explicitly provided
+        dtype = get_default_dtype(object)
+    return original_jnp_array(object, dtype=dtype, **kwargs)
 
 
-class BitContext64(BitContext):
+def patched_asarray(object, dtype=None, **kwargs):
+    if dtype is None:  # Only override if dtype is not explicitly provided
+        dtype = get_default_dtype(object)
+    return original_jnp_asarray(object, dtype=dtype, **kwargs)
 
-    def __init__(self):
-        super().__init__(64)
+
+# Patch jnp.array and jnp.asarray with our custom versions
+jnp.array = patched_array
+jnp.asarray = patched_asarray
--- dsa2000_cal/dsa2000_cal/common/tests/test_bit_context.py
+++ dsa2000_cal/dsa2000_cal/common/tests/test_bit_context.py
@@ -1,39 +1,118 @@
-from jax import numpy as jnp
+import jax
+import jax.numpy as jnp
 
-from dsa2000_cal.common.bit_context import BitContext64, BitContext32
+from dsa2000_cal.common.bit_context import BitContext
+
+jax.config.update("jax_enable_x64", True)
 
 
 def test_bit_context():
-    with BitContext64():
-        a = jnp.asarray(0.)
+    with BitContext(64):
+        a = jnp.array(0.)
         assert a.dtype == jnp.float64
 
         # Explicitly specifying float32 bypasses context
-        b = jnp.asarray(0., jnp.float32)
+        b = jnp.array(0., dtype=jnp.float32)
         assert b.dtype == jnp.float32
 
-        # Nesting
-        with BitContext32():
-            a = jnp.asarray(0.)
-            assert a.dtype == jnp.float32
-
-            # Explicitly specifying float64 cannot bypass context
-            b = jnp.asarray(0., jnp.float64)
-            assert b.dtype == jnp.float32
-
-    with BitContext32():
-        a = jnp.asarray(0.)
+    with BitContext(32):
+        a = jnp.array(0.)
         assert a.dtype == jnp.float32
 
-        # Explicitly specifying float64 cannot bypass context
-        b = jnp.asarray(0., jnp.float64)
-        assert b.dtype == jnp.float32
+        # Explicitly specifying float64 bypasses context
+        b = jnp.array(0., dtype=jnp.float64)
+        assert b.dtype == jnp.float64
 
-        # Nesting
-        with BitContext64():
-            a = jnp.asarray(0.)
-            assert a.dtype == jnp.float64
+    # test with int32 and int64
+    with BitContext(64):
+        a = jnp.array(0)
+        assert a.dtype == jnp.int64
 
-            # Explicitly specifying float32 bypasses context
-            b = jnp.asarray(0., jnp.float32)
-            assert b.dtype == jnp.float32
+        b = jnp.array(0, dtype=jnp.int32)
+        assert b.dtype == jnp.int32
+
+    with BitContext(32):
+        a = jnp.array(0)
+        assert a.dtype == jnp.int32
+
+        b = jnp.array(0, dtype=jnp.int64)
+        assert b.dtype == jnp.int64
+
+
+def test_bit_context_under_jit():
+    @jax.jit
+    def add64():
+        with BitContext(64):
+            x = jnp.array(1.)
+            y = jnp.array(2.)
+            return x + y
+
+    @jax.jit
+    def add32():
+        with BitContext(32):
+            x = jnp.array(1.)
+            y = jnp.array(2.)
+            return x + y
+
+    assert add64().dtype == jnp.float64
+
+    assert add32().dtype == jnp.float32
+
+    @jax.jit
+    def nested64():
+        with BitContext(32):
+            with BitContext(64):
+                x = jnp.array(1.)
+                y = jnp.array(2.)
+                return x + y
+
+    assert nested64().dtype == jnp.float64
+
+    @jax.jit
+    def nested32():
+        with BitContext(64):
+            with BitContext(32):
+                x = jnp.array(1.)
+                y = jnp.array(2.)
+                return x + y
+
+    assert nested32().dtype == jnp.float32
+
+    # Test for int32 and int64 too
+
+    @jax.jit
+    def add64_int():
+        with BitContext(64):
+            x = jnp.array(1)
+            y = jnp.array(2)
+            return x + y
+
+    @jax.jit
+    def add32_int():
+        with BitContext(32):
+            x = jnp.array(1)
+            y = jnp.array(2)
+            return x + y
+
+    assert add64_int().dtype == jnp.int64
+    assert add32_int().dtype == jnp.int32
+
+    @jax.jit
+    def nested64_int():
+        with BitContext(32):
+            with BitContext(64):
+                x = jnp.array(1)
+                y = jnp.array(2)
+                return x + y
+
+    assert nested64_int().dtype == jnp.int64
+
+    @jax.jit
+    def nested32_int():
+        with BitContext(64):
+            with BitContext(32):
+                x = jnp.array(1)
+                y = jnp.array(2)
+                return x + y
+
+    assert nested32_int().dtype == jnp.int32
