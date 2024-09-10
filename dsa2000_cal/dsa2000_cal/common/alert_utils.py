086403a2b0f290b29a8bcc98096e98d41570183d
joshuaalbert <albert@strw.leidenuniv.nl>
2024-09-10 10:24:55 +0200

    * add caller info

--- dsa2000_cal/dsa2000_cal/common/alert_utils.py
+++ dsa2000_cal/dsa2000_cal/common/alert_utils.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 import os
 import socket
@@ -5,6 +6,29 @@ import warnings
 from datetime import datetime, timedelta
 
 import requests
+
+
+def get_grandparent_info(relative_depth: int = 0):
+    """
+    Get the file, line number and function name of the caller of the caller of this function.
+
+    Args:
+        relative_depth: the number of frames to go back from the caller of this function. Default is 0.
+            This is interpreted as the number of frames to go back from the caller of the caller of this function.
+            0 means the caller of the caller of this function, 1 means the caller of the caller of the caller of this
+            function, and so on.
+
+    Returns:
+        str: a string with the file, line number and function name of the caller of the caller of this function.
+    """
+    # Get the grandparent frame (caller of the caller)
+    depth = min(1 + relative_depth, len(inspect.stack()) - 1)
+    caller_frame = inspect.stack()[depth]
+    caller_file = caller_frame.filename
+    caller_line = caller_frame.lineno
+    caller_func = caller_frame.function
+
+    return f"at {caller_file}:{caller_line} in {caller_func}"
 
 
 def post_completed_forward_modelling_run(run_dir: str, start_time: datetime, duration: timedelta,
