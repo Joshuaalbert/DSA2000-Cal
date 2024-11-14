e050c985ccfed034a7e452518f4f0496d5be0597
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-14 10:48:58 +0100

    Use expand dims

--- dsa2000_cal/src/dsa2000_cal/common/pure_callback_utils.py
+++ dsa2000_cal/src/dsa2000_cal/common/pure_callback_utils.py
@@ -40,7 +40,19 @@ def _build_callback_from_kernel(cb_kernel: Callable, batch_shape_determiner: Cal
 
         def sliced_kernel(index):
             multi_idx = np.unravel_index(index, batch_shape)
-            args_slice = jax.tree.map(lambda x: x[multi_idx], args)
+
+            def _slice(x):
+                if x is None:
+                    return x
+                _multi_idx = []
+                for idx, s in (zip(multi_idx, np.shape(x))):
+                    if s == 1:
+                        _multi_idx.append(0)
+                    else:
+                        _multi_idx.append(idx)
+                return x[tuple(_multi_idx)]
+
+            args_slice = jax.tree.map(_slice, args)
             return cb_kernel(*args_slice)
 
         if num_threads is not None:
@@ -64,6 +76,7 @@ def _build_batch_shape_determiner(*args_shape_size):
     def batch_shape_determiner(*args):
         if len(args) != len(args_shape_size):
             raise ValueError(f'Expected {len(args_shape_size)} arguments, got {len(args)}.')
+
         def _determine(x, shape_size):
             if not isinstance(shape_size, int):
                 raise ValueError(f'shape_size must be an integer, got {type(shape_size)}.')
@@ -72,24 +85,31 @@ def _build_batch_shape_determiner(*args_shape_size):
             if shape_size == 0:
                 return np.shape(x)
             return np.shape(x)[:-shape_size]
+
         batch_shapes = jax.tree.map(_determine, args, args_shape_size, is_leaf=lambda x: x is None)
 
         def is_leaf(s):
             # if tuple of int then it is a leaf
             return isinstance(s, tuple) and all(isinstance(i, int) for i in s)
+
         leaves = jax.tree.leaves(batch_shapes, is_leaf=is_leaf)
         shapes = set(leaves)
         # remove None
         shapes.discard(None)
-        if len(shapes) > 1:
-            raise ValueError(f'Inconsistent batch shapes: {shapes}')
-        return list(leaves)[0]
+        # broadcast
+        try:
+            batch_shape = np.broadcast_shapes(*list(shapes))
+        except ValueError as e:
+            if "shape mismatch" in str(e):
+                raise ValueError(f'Inconsistent batch shapes: {shapes}')
+            raise e
+        return batch_shape
 
     return batch_shape_determiner
 
 
 def construct_threaded_pure_callback(cb_kernel: Callable, result_shape_dtypes: Any, *args_shape_size,
-                                     num_threads: int | None = None):
+                                     num_threads: int | None = None, vmap_method='expand_dims'):
     """
     Construct a pure callback with vmap using threading.
 
@@ -101,6 +121,8 @@ def construct_threaded_pure_callback(cb_kernel: Callable, result_shape_dtypes: A
         *args_shape_size: the number of (unbatched) dimensions for each argument to cb_kernel.
         num_threads: the number of threads to use. If None, reuses a shared global threadpool, by default using all
             available CPUs, and which can be configured with environment variable `JAX_PURE_CALLBACK_NUM_THREADS`.
+        vmap_method: the vmap method to use. Must be one of 'expand_dims' or 'broadcast_all'. Default is 'expand_dims'.
+            See jax.pure_callback for more information.
 
     Returns:
         A pure callback that works with vmap, using threading to parallelize the computation.
@@ -113,6 +135,7 @@ def construct_threaded_pure_callback(cb_kernel: Callable, result_shape_dtypes: A
             if len(np.shape(x)) != shape_size:
                 raise ValueError(
                     f'Expected shape of size {shape_size} but got {np.shape(x)}, sized ({len(np.shape(x))}).')
+
         jax.tree.map(_check_shape, args, args_shape_size, is_leaf=lambda x: x is None)
         return cb_kernel(*args)
 
@@ -120,6 +143,6 @@ def construct_threaded_pure_callback(cb_kernel: Callable, result_shape_dtypes: A
     cb = _build_callback_from_kernel(wrapped_cb_kernel, batch_shape_determiner, num_threads=num_threads)
 
     def callback(*args):
-        return jax.pure_callback(cb, result_shape_dtypes, *args, vmap_method='broadcast_all')
+        return jax.pure_callback(cb, result_shape_dtypes, *args, vmap_method=vmap_method)
 
     return callback
