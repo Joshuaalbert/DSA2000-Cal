d548d32897f3c819bbccbfc12b189fb63f6dd85e
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-18 17:49:07 +0100

    * update

--- workflows/streaming_fm_distributed/run.sh
+++ workflows/streaming_fm_distributed/run.sh
@@ -53,7 +53,7 @@ cat "$TEMP_ENV_FILE"
 
 # Use the temporary .env file in Docker Compose commands
 docker compose --env-file "$TEMP_ENV_FILE" -f "$SCRIPT_DIR/docker-compose.yaml" down
-docker compose --env-file "$TEMP_ENV_FILE" --no-cache -f "$SCRIPT_DIR/docker-compose.yaml" build
+docker compose --env-file "$TEMP_ENV_FILE" -f "$SCRIPT_DIR/docker-compose.yaml" build --no-cache
 docker compose --env-file "$TEMP_ENV_FILE" config
 docker compose --env-file "$TEMP_ENV_FILE" -f "$SCRIPT_DIR/docker-compose.yaml" up -d
 
