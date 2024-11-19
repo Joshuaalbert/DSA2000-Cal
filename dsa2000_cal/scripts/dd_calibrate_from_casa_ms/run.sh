9c59f353b978c362a8ac1999d26cd85f7b0ff33a
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-20 00:26:23 +0100

    update

--- dsa2000_cal/scripts/dd_calibrate_from_casa_ms/run.sh
+++ dsa2000_cal/scripts/dd_calibrate_from_casa_ms/run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# copy local with rsync -avP
 PREFIX=/lustre/rbyrne/2024-03-03
+rsync -avP ${PREFIX}/20240303_133205_73MHz.ms ${LOCAL_PREFIX}/20240303_133205_73MHz.ms
+rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Cas.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cas.ms
+rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Cyg.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cyg.ms
+rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Vir.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Vir.ms
+rsync -avP ${PREFIX}/20240303_133205_73MHz_model_diffuse.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_diffuse.ms
+
+LOCAL_PREFIX=$PWD
 
 python main.py \
-  --data_ms="${PREFIX}/20240303_133205_73MHz.ms" \
-  --subtract_ms_list="${PREFIX}/20240303_133205_73MHz_model_Cas.ms,${PREFIX}/20240303_133205_73MHz_model_Cyg.ms,${PREFIX}/20240303_133205_73MHz_model_Vir.ms" \
-  --no_subtract_ms_list="${PREFIX}/20240303_133205_73MHz_model_diffuse.ms" \
+  --data_ms="${LOCAL_PREFIX}/20240303_133205_73MHz.ms" \
+  --subtract_ms_list="${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cas.ms,${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cyg.ms,${LOCAL_PREFIX}/20240303_133205_73MHz_model_Vir.ms" \
+  --no_subtract_ms_list="${LOCAL_PREFIX}/20240303_133205_73MHz_model_diffuse.ms" \
   --times_per_chunk=1
+
