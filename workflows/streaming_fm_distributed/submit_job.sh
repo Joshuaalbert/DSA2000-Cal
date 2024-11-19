facaa7c27930a4f1278d8cda58bf0ae9f33ea009
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-19 12:26:58 +0100

    * update

--- workflows/streaming_fm_distributed/submit_job.sh
+++ workflows/streaming_fm_distributed/submit_job.sh
@@ -2,4 +2,8 @@
 
 SERVICE_NAME="ray"
 
-docker compose exec $SERVICE_NAME /dsa/code/src/submit_job.sh
+#docker compose exec $SERVICE_NAME /dsa/code/src/submit_job.sh
+
+docker exec -d $SERVICE_NAME /dsa/code/src/submit_job.sh
+
+docker logs -f $SERVICE_NAME
