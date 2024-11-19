c51b0ac7a03b09ed8328b5460d1b31b269c4a3eb
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-20 00:28:22 +0100

    update

--- dsa2000_cal/scripts/dd_calibrate_from_casa_ms/run.sh
+++ dsa2000_cal/scripts/dd_calibrate_from_casa_ms/run.sh
@@ -2,13 +2,14 @@
 
 # copy local with rsync -avP
 PREFIX=/lustre/rbyrne/2024-03-03
+LOCAL_PREFIX=$PWD
 rsync -avP ${PREFIX}/20240303_133205_73MHz.ms ${LOCAL_PREFIX}/20240303_133205_73MHz.ms
 rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Cas.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cas.ms
 rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Cyg.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Cyg.ms
 rsync -avP ${PREFIX}/20240303_133205_73MHz_model_Vir.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_Vir.ms
 rsync -avP ${PREFIX}/20240303_133205_73MHz_model_diffuse.ms ${LOCAL_PREFIX}/20240303_133205_73MHz_model_diffuse.ms
 
-LOCAL_PREFIX=$PWD
+
 
 python main.py \
   --data_ms="${LOCAL_PREFIX}/20240303_133205_73MHz.ms" \
