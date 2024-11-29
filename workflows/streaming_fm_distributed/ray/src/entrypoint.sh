3fdd2ac4c6ea138eac96d926dec5598e8779443d
joshuaalbert <albert@strw.leidenuniv.nl>
2024-11-29 15:39:36 +0100

    update

--- workflows/streaming_fm_distributed/ray/src/entrypoint.sh
+++ workflows/streaming_fm_distributed/ray/src/entrypoint.sh
@@ -38,6 +38,8 @@ echo "PACKAGE_DIR=${PACKAGE_DIR}"
 LARGE_N=99999999999
 export RAY_health_check_initial_delay_ms=$LARGE_N
 export RAY_health_check_period_ms=$LARGE_N
+export RAY_SERVE_QUEUE_LENGTH_RESPONSE_DEADLINE_S=$LARGE_N
+export SLOW_STARTUP_WARNING_PERIOD_S=$LARGE_N
 
 if [ -z "$IS_RAY_HEAD" ]; then
   echo "Error: IS_RAY_HEAD must be specified."
