6a4e332c34b40c0708501644882a93f965e6c758
joshuaalbert <albert@strw.leidenuniv.nl>
2024-12-05 12:34:16 +0100

    clean

--- dsa2000_cal/src/dsa2000_cal/common/noise.py
+++ dsa2000_cal/src/dsa2000_cal/common/noise.py
@@ -1,12 +1,5 @@
-from typing import List
-
 import jax
 import jax.numpy as jnp
-import numpy as np
-from pyrap import tables as pt
-
-from dsa2000_cal.assets.arrays.array import AbstractArray
-from dsa2000_cal.common.quantity_utils import quantity_to_jnp
 
 
 def calc_baseline_noise(system_equivalent_flux_density: float | jax.Array, chan_width_hz: float | jax.Array,
@@ -47,34 +40,3 @@ def calc_image_noise(system_equivalent_flux_density: float, bandwidth_hz: float,
         system_equivalent_flux_density=system_equivalent_flux_density,
         chan_width_hz=bandwidth_hz,
         t_int_s=t_int_s) / jnp.sqrt(2 * num_baselines)  # 2 is for real component only
-
-
-def sum_and_add_noise(output_ms_file: str, input_ms_files: List[str], array: AbstractArray,
-                      channel_width_hz: float, integration_time_s: float):
-    """
-    Sum the visibilities in the input measurement sets and add noise to the output measurement set.
-
-    Args:
-        output_ms_file: the output measurement set file
-        input_ms_files: the input measurement set files
-        array: the array object
-    """
-    noise_sigma = calc_baseline_noise(
-        system_equivalent_flux_density=quantity_to_jnp(array.get_system_equivalent_flux_density(), 'Jy'),
-        chan_width_hz=channel_width_hz,
-        t_int_s=integration_time_s
-    )
-    print(f"Adding noise with sigma {noise_sigma} to {output_ms_file}")
-
-    with pt.table(output_ms_file, readonly=False) as output_ms:
-        shape = output_ms.getcol('DATA').shape
-        dtype = output_ms.getcol('DATA').dtype
-        # Divide noise scale by sqrt(2) to account for real and imaginary parts.
-        output_with_noise = (
-                np.random.normal(loc=0, scale=noise_sigma / np.sqrt(2.), size=shape).astype(dtype)
-                + 1j * np.random.normal(loc=0, scale=noise_sigma / np.sqrt(2.), size=shape).astype(dtype)
-        )
-        for input_ms_file in input_ms_files:
-            with pt.table(input_ms_file) as input_ms:
-                output_with_noise += input_ms.col('DATA')[:]
-        output_ms.putcol('DATA', output_with_noise)
