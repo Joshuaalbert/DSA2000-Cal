22f7f4ed47d663cf9badda112f77c64571eef599
joshuaalbert <albert@strw.leidenuniv.nl>
2024-12-19 19:48:32 +0100

    * fix content

--- dsa2000_cal/src/dsa2000_cal/assets/array_constraints/array_constraint_content.py
+++ dsa2000_cal/src/dsa2000_cal/assets/array_constraints/array_constraint_content.py
@@ -3,7 +3,6 @@ from abc import ABC
 from functools import cached_property
 from typing import Tuple, List, Callable
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import uniform
@@ -227,6 +226,70 @@ class ArrayConstraint(ABC, BaseContent):
 
         return [
             (RegionSampler(os.path.join(folder, "Spring Valley AOI 3.1b.shp")), 20.0)
+        ]
+
+
+class ArrayConstraintV2(ABC, BaseContent):
+    """
+    Abstract array class.
+    """
+
+    def __init__(self, *args, **kwargs):
+        BaseContent.__init__(self, seed='array_constraint_v2')
+
+    def get_array_constraint_folder(self) -> str:
+        return str(os.path.join(*self.content_path, 'spring_valley_31b_v2'))
+
+    def get_constraint_regions(self) -> List[Tuple[RegionSampler, float]]:
+        """
+        Returns the buffer constraints for the array.
+
+        Returns:
+            list of tuples: (RegionSampler, buffer distance m)
+        """
+        _30ft = 9.144
+        _50ft = 15.24
+        _60ft = 18.288
+        _100ft = 30.48
+        _200ft = 60.96
+
+        folder = self.get_array_constraint_folder()
+
+        return [
+            (RegionSampler(os.path.join(folder, "aoi_NHD_High_Resolution_Flowline.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "aoi_NHD_V2_Flowlines.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "aoi_NHD_V2_Waterbodies.shp")), _200ft),
+            (RegionSampler(os.path.join(folder, "aoi_NWI_Wetland.shp")), _200ft),
+            (RegionSampler(os.path.join(folder, "Avoidance_Area_9_9.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Avoidance - Terrain.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Existing Access Paths.shp")), _30ft),
+            (RegionSampler(os.path.join(folder, "Fences.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Private Property within AOI.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Soil Data - 3_0.shp"),
+                           filter=lambda gdf: gdf[gdf['SUMMARY_TY'] == "Bedrock"]), _200ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Ephemeral Streams.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Intermittent Streams.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Perennial Streams- Observed.shp")), _60ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Road Segments.shp")), _30ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Water Bodies.shp")), _200ft),
+            (RegionSampler(os.path.join(folder, "Spring Valley Watershed.shp")), _200ft),
+            (RegionSampler(os.path.join(folder, "Tertiary Roads.shp")), _30ft)
+        ]
+
+    def get_area_of_interest_regions(self) -> List[Tuple[RegionSampler, float]]:
+        """
+        Returns the buffer constraints for the array.
+
+        Returns:
+            list of tuples: (RegionSampler, buffer distance m)
+        """
+        # Prepend the folder path
+        folder = self.get_array_constraint_folder()
+
+        return [
+            (RegionSampler(os.path.join(folder, "Spring Valley AOI 3.1b.shp")), 20.0),
+            # (RegionSampler(os.path.join(folder, "Buildable Area - Outside AOI.shp")), 20.0),
+            # (RegionSampler(os.path.join(folder, "Western Expansion Areas.shp")), 20.0)
         ]
 
 
