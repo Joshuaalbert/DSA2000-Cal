0aaf252a5b159e27187e8eaff1259490977409f1
joshuaalbert <albert@strw.leidenuniv.nl>
2025-02-10 13:44:34 -0800

    * fix

--- dsa2000_cal/src/dsa2000_cal/gain_models/tests/test_beam_gain_model.py
+++ dsa2000_cal/src/dsa2000_cal/gain_models/tests/test_beam_gain_model.py
@@ -20,7 +20,8 @@ def test_pytree_serialisation():
     beam_gain_model = block_until_ready(f(beam_gain_model))
 
 
-@pytest.mark.parametrize('array_name', ['lwa_mock', 'dsa2000W_small'])
+# @pytest.mark.parametrize('array_name', ['lwa_mock', 'dsa2000W_small'])
+@pytest.mark.parametrize('array_name', ['dsa2000W'])
 def test_beam_gain_model_factory(array_name: str):
     t0 = time_mod.time()
     beam_gain_model = build_beam_gain_model(array_name=array_name)
