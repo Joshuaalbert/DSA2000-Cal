898c5d2800c030c10ebceeca942a05fa66d9a0b6
joshuaalbert <albert@strw.leidenuniv.nl>
2025-02-14 17:55:48 -0800

    * cleanup rcp

--- dsa2000_cal/benchmarking/wgridder_predict/main.py
+++ dsa2000_cal/benchmarking/wgridder_predict/main.py
@@ -10,13 +10,9 @@ from tomographic_kernel.frames import ENU
 from dsa2000_cal.common.quantity_utils import quantity_to_jnp, time_to_jnp
 from dsa2000_cal.common.types import VisibilityCoords
 from dsa2000_common.delay_models.base_far_field_delay_engine import build_far_field_delay_engine
-
-
 from dsa2000_common.delay_models.base_near_field_delay_engine import build_near_field_delay_engine
-
-
 from dsa2000_common.geodesics.base_geodesic_model import build_geodesic_model
-from dsa2000_common.visibility_model.source_models import build_fits_source_model
+from dsa2000_common.visibility_model.source_models.celestial.base_fits_source_model import build_fits_source_model
 
 
 def build_mock_obs_setup(ant: int, time: int, num_freqs: int):
