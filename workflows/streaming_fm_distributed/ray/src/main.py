088c97fa9c7aa48eadfacaed92fb17ebad5acb76
vrosero <vrosero@caltech.edu>
2025-03-13 21:44:52 -0700

    changing duration in main.py

--- workflows/streaming_fm_distributed/ray/src/main.py
+++ workflows/streaming_fm_distributed/ray/src/main.py
@@ -64,7 +64,7 @@ def build_run_params(array_name: str, with_autocorr: bool, field_of_view: au.Qua
 
     num_channels = len(freqs)
 
-    channel_width = 500 * array.get_channel_width(). # 500x 130 kHz = 65 MHz
+    channel_width = 500 * array.get_channel_width() # 500x 130 kHz = 65 MHz
     solution_interval_freq = num_freqs_per_sol_int * channel_width
     sub_band_interval = channel_width * num_channels / num_sub_bands
     num_sol_ints_per_sub_band = int(sub_band_interval / solution_interval_freq)
