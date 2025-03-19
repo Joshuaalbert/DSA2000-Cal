b07a713c16ef27ef4b305a738fbf52a8f60d3f69
joshuaalbert <albert@strw.leidenuniv.nl>
2025-03-19 18:44:44 +0100

    * run as user

--- workflows/streaming_fm_distributed/run.sh
+++ workflows/streaming_fm_distributed/run.sh
@@ -33,9 +33,7 @@ fi
 # Use a subshell so that the exported variables are not available after the script finishes
 (
 
-  UID=$(id -u)
   GID=$(id -g)
-  export UID
   export GID
 
   export NODE_NAME
