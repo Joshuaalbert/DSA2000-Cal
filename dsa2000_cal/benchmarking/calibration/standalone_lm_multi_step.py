925fd9441d34a6d98e1c0dc79494cbb53d038444
vrosero <vrosero@caltech.edu>
2025-03-23 16:07:02 -0700

    fix import fns for MultiStepLevenbergMarquardt

--- dsa2000_cal/benchmarking/calibration/standalone_lm_multi_step.py
+++ dsa2000_cal/benchmarking/calibration/standalone_lm_multi_step.py
@@ -21,8 +21,8 @@ if not jax.config.read('jax_enable_x64'):
     jax.config.update('jax_enable_x64', True)
 
 # Create a float scalar to lock in dtype choices.
-if jnp.array(1., jnp.float64).dtype != jnp.float32:
-    raise RuntimeError("Failed to set float64 as default dtype.")
+# if jnp.array(1., jnp.float64).dtype != jnp.float32:
+#     raise RuntimeError("Failed to set float64 as default dtype.")
 
 PRNGKey = jax.Array
 Array = Union[
