3cc08d0f2ddd2b348353612dedec5a0395848d21
joshuaalbert <albert@strw.leidenuniv.nl>
2025-03-31 16:59:34 +0200

    * fix benchmark

--- dsa2000_cal/benchmarking/calibration/benchmark_gpu.py
+++ dsa2000_cal/benchmarking/calibration/benchmark_gpu.py
@@ -1,13 +1,12 @@
 import os
 from functools import partial
 
-from dsa2000_cal.calibration_step import calibration_step
-from dsa2000_cal.probabilistic_models.gain_prior_models import GainPriorModel
-
 os.environ['JAX_PLATFORMS'] = 'cuda,cpu'
 os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '1.0'
 os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={8}"
 
+from dsa2000_cal.calibration_step import calibration_step
+from dsa2000_cal.probabilistic_models.gain_prior_models import GainPriorModel
 from dsa2000_cal.solvers.cg import tree_add, tree_scalar_mul
 from dsa2000_common.common.jvp_linear_op import JVPLinearOp
 from jax._src.partition_spec import PartitionSpec
@@ -27,7 +26,7 @@ import numpy as np
 from dsa2000_cal.ops.residuals import compute_residual_TBC
 
 
-def prepare_data_cal(D: int, T, C, Ts, Tm, Cs, Cm) -> Dict[str, Any]:
+def prepare_data_cal(D: int, T, C, BTs, BTm, BCs, BCm) -> Dict[str, Any]:
     num_antennas = 2048
     baseline_pairs = np.asarray(list(itertools.combinations(range(num_antennas), 2)),
                                 dtype=np.int32)
@@ -39,15 +38,15 @@ def prepare_data_cal(D: int, T, C, Ts, Tm, Cs, Cm) -> Dict[str, Any]:
     antenna2 = antenna2[sort_idxs]
 
     B = antenna1.shape[0]
-    vis_model = np.zeros((D, T // Tm, B, C // Cm, 2, 2), dtype=mp_policy.vis_dtype)
-    vis_data = np.zeros((T // Tm, B, C // Cm, 2, 2), dtype=mp_policy.vis_dtype)
-    weights = np.ones((T // Tm, B, C // Cm, 2, 2), dtype=mp_policy.weight_dtype)
+    vis_model = np.zeros((D, T // BTm, B, C // BCm, 2, 2), dtype=mp_policy.vis_dtype)
+    vis_data = np.zeros((T // BTm, B, C // BCm, 2, 2), dtype=mp_policy.vis_dtype)
+    weights = np.ones((T // BTm, B, C // BCm, 2, 2), dtype=mp_policy.weight_dtype)
 
     gain_probabilistic_model = GainPriorModel(
         num_source=D,
         num_ant=num_antennas,
-        freqs=np.linspace(700e6, 800e6, Cm),
-        times=np.linspace(0., 6, Tm),
+        freqs=np.linspace(700e6, 800e6, C // BCs),
+        times=np.linspace(0., 6, T // BTs),
         gain_stddev=2.,
         full_stokes=True,
         dd_type='unconstrained',
@@ -78,7 +77,8 @@ def build_sharded_entry_point_cal(backend):
         gain_probabilistic_model = data['gain_probabilistic_model']
 
         return calibration_step(
-            params=None, vis_model=vis_model, vis_data=vis_data, weights=weights,
+            params=None, vis_model=vis_model,
+            vis_data=vis_data, weights=weights,
             antenna1=antenna1, antenna2=antenna2,
             gain_probabilistic_model=gain_probabilistic_model, verbose=False,
             backend=backend, num_B_shards=num_devices, num_C_shards=1, num_devices=num_devices,
@@ -88,14 +88,14 @@ def build_sharded_entry_point_cal(backend):
     return entry_point
 
 
-def run_cal(T, C, Ts, Tm, Cs, Cm, backend, m, task, scheme):
+def run_cal(T, C, BTs, BTm, BCs, BCm, backend, m, task, scheme):
     sharded_entry_point = build_sharded_entry_point_cal(backend)
     sharded_entry_point_jit = jax.jit(sharded_entry_point)
     # Run benchmarking over number of calibration directions
     shard_time_array = []
     d_array = []
     for D in range(1, 9):
-        data = prepare_data_cal(D, T, C, Ts, Tm, Cs, Cm)
+        data = prepare_data_cal(D, T, C, BTs, BTm, BCs, BCm)
         sharded_entry_point_jit_compiled = sharded_entry_point_jit.lower(data).compile()
         t0 = time.time()
         for _ in range(m):
@@ -113,9 +113,9 @@ def run_cal(T, C, Ts, Tm, Cs, Cm, backend, m, task, scheme):
     dsa_logger.info(f"{task} {scheme}: {backend}: t(n) = {a:.4f} * n ** {b:.2f} + {c:.4f}")
 
 
-def prepare_data(D: int, T, C, Ts, Tm, Cs, Cm) -> Dict[str, Any]:
-    assert T % Tm == 0 and T % Ts == 0
-    assert C % Cm == 0 and C % Cs == 0
+def prepare_data(D: int, T, C, BTs, BTm, BCs, BCm) -> Dict[str, Any]:
+    assert T % BTm == 0 and T % BTs == 0
+    assert C % BCm == 0 and C % BCs == 0
 
     num_antennas = 2048
     baseline_pairs = np.asarray(list(itertools.combinations(range(num_antennas), 2)),
@@ -128,11 +128,11 @@ def prepare_data(D: int, T, C, Ts, Tm, Cs, Cm) -> Dict[str, Any]:
     antenna2 = antenna2[sort_idxs]
 
     B = antenna1.shape[0]
-    vis_model = np.zeros((D, T // Tm, B, C // Cm, 2, 2), dtype=mp_policy.vis_dtype)
-    vis_data = np.zeros((T // Tm, B, C // Cm, 2, 2), dtype=mp_policy.vis_dtype)
-    gains = np.zeros((D, T // Ts, num_antennas, C // Cs, 2, 2), dtype=mp_policy.gain_dtype)
+    vis_model = np.zeros((D, T // BTm, B, C // BCm, 2, 2), dtype=mp_policy.vis_dtype)
+    vis_data = np.zeros((T // BTm, B, C // BCm, 2, 2), dtype=mp_policy.vis_dtype)
+    gains = np.zeros((D, T // BTs, num_antennas, C // BCs, 2, 2), dtype=mp_policy.gain_dtype)
 
-    dsa_logger.info(f"Model size {vis_data.nbytes / 2 ** 20} MB, gain size {gains.nbytes / 2 ** 20} MB")
+    dsa_logger.info(f"Model size D * {vis_data.nbytes / D / 2 ** 20} MB, gain size D * {gains.nbytes / D / 2 ** 20} MB")
 
     return dict(
         vis_model=vis_model,
@@ -244,7 +244,7 @@ def build_sharded_entry_point_JtJp(devices):
     return entry_point_sharded, mesh
 
 
-def run(build_sharded_entry_point_fn, T, C, Ts, Tm, Cs, Cm, backend, m, task, scheme):
+def run(build_sharded_entry_point_fn, T, C, BTs, BTm, BCs, BCm, backend, m, task, scheme):
     devices = jax.devices(backend)
     sharded_entry_point, mesh = build_sharded_entry_point_fn(devices)
     sharded_entry_point_jit = jax.jit(sharded_entry_point)
@@ -252,7 +252,7 @@ def run(build_sharded_entry_point_fn, T, C, Ts, Tm, Cs, Cm, backend, m, task, sc
     shard_time_array = []
     d_array = []
     for D in range(1, 9):
-        data = prepare_data(D, T, C, Ts, Tm, Cs, Cm)
+        data = prepare_data(D, T, C, BTs, BTm, BCs, BCm)
         sharded_entry_point_jit_compiled = sharded_entry_point_jit.lower(data).compile()
         t0 = time.time()
         for _ in range(m):
@@ -274,24 +274,24 @@ def main():
     for task, build_sharded_entry_point_fn in zip(
             ['R(x)', 'J^T.R(x)', 'J^T.J.p'],
             [build_sharded_entry_point_R, build_sharded_entry_point_JtR, build_sharded_entry_point_JtJp]):
-        for scheme, (T, C, Ts, Tm, Cs, Cm) in zip(
+        for scheme, (T, C, BTs, BTm, BCs, BCm) in zip(
                 ['Full-Avg. per-GPU', 'Non-Avg. per-GPU', 'Non-Avg. all-GPU'],
                 [
                     (4, 4, 4, 4, 4, 4),
-                    (4, 4, 4, 4, 1, 1),
-                    (4, 40, 4, 40, 1, 1)
+                    (4, 4, 4, 1, 4, 1),
+                    (4, 40, 4, 1, 40, 1)
                 ]
         ):
             for backend in ['cpu', 'cuda']:
-                run(build_sharded_entry_point_fn, T=T, C=C, Ts=Ts, Tm=Tm, Cs=Cs, Cm=Cm, backend=backend, m=10,
+                run(build_sharded_entry_point_fn, T=T, C=C, BTs=BTs, BTm=BTm, BCs=BCs, BCm=BCm, backend=backend, m=10,
                     task=task, scheme=scheme)
                 run_cal(
                     T=T,
                     C=C,
-                    Ts=Ts,
-                    Cs=Cs,
-                    Tm=Tm,
-                    Cm=Cm,
+                    BTs=BTs,
+                    BCs=BCs,
+                    BTm=BTm,
+                    BCm=BCm,
                     backend=backend,
                     m=10,
                     task=task,
