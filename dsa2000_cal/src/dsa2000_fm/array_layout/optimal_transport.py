1758c53efacdc825c9af8980263b1e9311eab939
joshuaalbert <albert@strw.leidenuniv.nl>
2025-04-25 01:57:14 +0200

    * fix

--- dsa2000_cal/src/dsa2000_fm/array_layout/optimal_transport.py
+++ dsa2000_cal/src/dsa2000_fm/array_layout/optimal_transport.py
@@ -265,7 +265,7 @@ def compute_ideal_uv_distribution(du: au.Quantity, R: au.Quantity, target_fwhm: 
     gamma = quantity_to_np(compute_uv_gaussian_width(target_fwhm, freq), 'm')
     R_jax = quantity_to_np(R, 'm')
     du_jax = quantity_to_np(du, 'm')
-    uvec_bins = np.arange(-expand_factor * R_jax - du_jax, expand_factor * R_jax + du_jax, du_jax)
+    uvec_bins = np.arange(-R_jax - du_jax, R_jax + du_jax, du_jax)
     uvec = 0.5 * (uvec_bins[1:] + uvec_bins[:-1])
     U, V = np.meshgrid(uvec, uvec, indexing='ij')
     norm = np.reciprocal(2 * np.pi * gamma ** 2)
